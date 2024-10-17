afa203f7845a36ebddda8b8c9bd4b6421c601b8f
MadP <35908227+dinosaguindan@users.noreply.github.com>
2024-10-17 15:52:02 +0800

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,20 @@
+select * From customers;
+
+select * From items;
+
+select * From orders;
+
+select * From sales;
+
+
+select 
+    c.customer_id as Customer,
+    c.age as Age,
+    i.item_name as Item,
+    sum(quantity) as Quantity
+from customers c
+inner join sales s on c.customer_id = s.customer_id
+inner join orders o on s.sales_id = o.sales_id
+inner join items i on o.item_id = i.item_id
+group by c.customer_id ,c.age ,i.item_name
+having c.age between 18 and 35
